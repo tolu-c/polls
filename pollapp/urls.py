44ef9b3dddaed82c1c295153e6a862478920d9ff
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-01-07 04:56:17 +0100

    part 3

--- pollapp/urls.py
+++ pollapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'pollapp'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('question/<int:question_id>/', views.detail, name='detail'),
+    path('question/<int:question_id>/results/', views.results, name='results'),
+    path('question/<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- pollapp/views.py
+++ pollapp/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from pollapp.models import Question
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Welcome to my Polls app")
+    latest_question_list = Question.objects.order_by('-publish')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'pollapp/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pollapp/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = 'You are looking at the results of the questions %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
